9322850e4a160572a069a12ecf525786a2d81a9d
Brandon Smith <brandon.smith@yale.edu>
2013-03-29 11:16:22 -0400

    added tasks routes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = 'fabe96596dc79285c7ea762ea600976f6fc48ccd8157d13e1c9b81417dab76e2fd74414eb985a38c351a84db76d3022e2d5dfc93e7edc5f8a2b5a4de6a54515e'
