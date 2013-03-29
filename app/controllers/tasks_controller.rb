5b8a41806ed9da96be4c808c70d1fd13b4ac9555
Brandon Smith <brandon.smith@yale.edu>
2013-03-29 11:50:54 -0400

    updated tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,17 +4,30 @@ class TasksController < ApplicationController
 	end
 
 	def create
+		@task = Task.new(params[:task])
 	end
 
 	def new
+		@task = Task.new
 	end
 
 	def destroy
 	end
 
 	def show
+		@task = Task.find(params[:id])
+
+		respond_to do |format|
+     format.html
+     format.js
+   end
 	end
 
 	def edit
+		@task = Task.find(params[:id])
+	end
+	
+	def update
+		@task = update_attributes(params[:task])
 	end
 end
